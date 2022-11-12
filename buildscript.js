00369c644c5eaffc8bd389444a7ea429513c41ea
scriptPilot <dennis@scheffel.de>
2022-11-12 23:37:47 +0100

    code docu

--- buildscript.js
+++ buildscript.js
@@ -1,25 +1,39 @@
+// Import File System module
 const fs = require('fs')
 
+// Read library folder
 fs.readdir('lib', (err, files) => {
 
+  // Exit on error
   if (err) {
     console.error('Failed to loop the library folder.', err)
     process.exit(1)
   }
 
-  files = ['../credentials.template.js', ...files.filter(file => /\.js/.test(file))]
+  // Filter for .js files
+  files = files.filter(file => /\.js/.test(file))
 
+  // Add credentials template file
+  files = ['../credentials.template.js', ...files]
+
+  // Start array of code blocks
   const codeBlocks = []
 
+  // Loop files
   files.forEach(file => {
 
+    // Read file content
     const fileContent = fs.readFileSync('lib/' + file, { encoding: 'utf8' })
+
+    // Add file content to code blocks
     codeBlocks.push(fileContent)
 
   })
 
+  // Merge and write code blocks to Code.gs file
   fs.writeFileSync('Code.gs', codeBlocks.join('\n'))
 
+  // Log script completion
   console.log('Code.gs file updated.')
 
 })
